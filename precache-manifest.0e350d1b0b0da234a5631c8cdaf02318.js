bba673d3bca8d99eeb42f6b55d0660a04de1b346
devevangel <dev.evangel@gmail.com>
2020-10-26 17:21:50 +0100

    Updates

---
+++ precache-manifest.0e350d1b0b0da234a5631c8cdaf02318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fc9fa701c8db011689e5a9d239a80d",
+    "url": "/nobleman/index.html"
+  },
+  {
+    "revision": "82849050c8c66bdace8b",
+    "url": "/nobleman/static/css/main.b3c3f85d.chunk.css"
+  },
+  {
+    "revision": "a4156d47bfc09f5cb176",
+    "url": "/nobleman/static/js/2.a3a3faf7.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/nobleman/static/js/2.a3a3faf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82849050c8c66bdace8b",
+    "url": "/nobleman/static/js/main.86b89fb9.chunk.js"
+  },
+  {
+    "revision": "2d7c06b205a2bf0091ac",
+    "url": "/nobleman/static/js/runtime-main.5c83a3e0.js"
+  }
+]);
